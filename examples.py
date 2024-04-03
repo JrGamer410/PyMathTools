6fb716c3376bb1f14ecbaf0123915ce2804b50eb
JrGamer410 <91926668+JrGamer410@users.noreply.github.com>
2024-04-02 20:24:02 -0500

    Updated example script

--- examples.py
+++ examples.py
@@ -2,3 +2,7 @@ from pymathtools import *
 
 prime = isprime(17)
 print(f"isprime(17) Response: {prime}")
+rectanglearea = rectanglearea(7, 4)
+print(f"Rectangle with area of 7 and 4: {rectanglearea}")
+trianglearea = trianglearea(6, 3)
+print(f"Triangle with area of 6 and 3: {trianglearea}")
